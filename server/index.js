e7a669a3b8e2abe0fb2caee0ff588c47fc43a8fa
= <=>
2019-05-10 09:55:25 -0700

    environment variables, setup database

--- server/index.js
+++ server/index.js
@@ -1,12 +1,26 @@
+require('dotenv').config()
+
+//dependencies
 const express = require('express');
 const app = express();
-const PORT = 4000;
+const massive = require('massive')
+
+//env variables
+const { 
+    PORT,
+    CONNECTION_STRING
+} = process.env
 
 //controllers
 const controller = require('./controller')
 
-
-
+//database
+massive(CONNECTION_STRING)
+    .then(db => { 
+        console.log('connected to database')
+    }).catch(err => { 
+        console.log('failed to connect to database')
+    })
 
 
 app.listen(PORT, () => { 
